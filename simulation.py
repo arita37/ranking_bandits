4bc83fc587ae756ffcf9f72f0500020cba1ed8c2
yacinebouaouni <yacinebouaouni07@gmail.com>
2023-10-11 15:48:03 +0200

    added kendall tau metric

--- simulation.py
+++ simulation.py
@@ -5,13 +5,14 @@ import fire
 import pyinstrument
 from bandits_to_rank.opponents.top_rank import TOP_RANK
 from utilmy import (log, os_makedirs, config_load)
+from scipy.stats import kendalltau
 
 
 def binomial_sample(p: float, size: int = 1, n: int = 1):
     return np.random.binomial(n=n, p=p, size=size)
 
 
-def generate_click_data(cfg, T):
+def generate_click_data(cfg: str, T: int):
     """
     Generate a dataframe with sampled items and locations with binomial sampling
 
@@ -27,8 +28,10 @@ def generate_click_data(cfg, T):
         - is_clk: Binary reward (1 for click, 0 for no click) sampled from the given probabilities. (int)
     """
     data = []
+    cfg = config_load(cfg)
+    cfg = config_load(cfg['probas_json'])
     locations, items = list(cfg['loc_probas'].keys()), list(
-        cfg['item_probas'].keys())
+    cfg['item_probas'].keys())
     for ts in range(T):
         loc_id = np.random.choice(locations)
         item_id = np.random.choice(items)
@@ -36,12 +39,11 @@ def generate_click_data(cfg, T):
         item_prob = cfg['item_probas'][item_id][loc_id]
 
         is_clk = binomial_sample(item_prob*loc_prob)[0]
-        data.append([ts, loc_id, item_id, is_clk])
+        data.append([ts, int(loc_id), int(item_id), is_clk])
 
     df = pd.DataFrame(data, columns=['ts', 'loc_id', 'item_id', 'is_clk'])
     df.to_csv('data_simulation.csv', index=False)
     return df
-
 
 def test_toprank(cfg):
     """
@@ -58,7 +60,7 @@ def test_toprank(cfg):
     df = pd.read_csv(cfg['dataframe_csv'])
 
     nb_arms = len(df['item_id'].unique())
-    discount_factors = [0.9, 0.9, 0.9]
+    discount_factors = [0.5, 0.5, 0.5]
     T = len(df)
     player = TOP_RANK(nb_arms, T=T, discount_factor=discount_factors)
 
@@ -69,9 +71,29 @@ def test_toprank(cfg):
 
         # Simulate a game action and reward
         action_list, _ = player.choose_next_arm()
-        reward_list = np.where(np.arange(nb_arms) == int(
-            item_id[-1])-1, is_clk, np.zeros(nb_arms))
+        reward_list = np.where(np.arange(nb_arms) == item_id, is_clk, np.zeros(nb_arms))
         player.update(action_list, reward_list)
+
+    return player
+
+def evaluate_ranking_kendall(player, df):
+
+    clicked_items = df[df['is_clk'] == 1]
+
+    item_click_counts = clicked_items.groupby('item_id')['is_clk'].sum().reset_index()
+    ground_truth_ranking = item_click_counts.sort_values(by='is_clk', ascending=False)['item_id'].tolist()
+
+    action_list, _ = player.choose_next_arm()
+    kendall_tau, _ = kendalltau(ground_truth_ranking, action_list)
+    print(action_list, ground_truth_ranking)
+    return kendall_tau
+
+
+
+def main():
+    df = pd.read_csv('data_simulation.csv')
+    player = test_toprank("config.yaml")
+    print(f'score = {evaluate_ranking_kendall(player, df)}')
 
 
 if __name__ == "__main__":
@@ -79,8 +101,10 @@ if __name__ == "__main__":
         profiler = pyinstrument.Profiler()
         profiler.start()
 
-        fire.Fire()
+        fire.Fire(main)
         profiler.stop()
         print(profiler.output_text(unicode=True, color=True))
     else:
-        fire.Fire()
+        fire.Fire(main)
+
+
