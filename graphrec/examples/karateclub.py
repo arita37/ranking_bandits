4f1cd69e2c00e6f81b6b0b232ee9856d724a8cfd
nono <noelkev0@gmail.com>
2023-10-24 08:03:35 +0000

    ok

--- graphrec/examples/karateclub.py
+++ graphrec/examples/karateclub.py
@@ -12,7 +12,7 @@ import pathlib
 
 from fastrec import GraphRecommender
 
-from utilmy import (log, os_makedirs, pd_read_file)
+from utilmy import (log, os_makedirs, pd_read_file, pd_to_file)
 
 
 def animate(labelsnp, all_embeddings,mask, dirout="ztmp/"):
@@ -44,7 +44,13 @@ def animate(labelsnp, all_embeddings,mask, dirout="ztmp/"):
             writer.append_data(data)
 
 
-def run(epoch=2):
+def run(epoch=2, debug=1, dirout="ztmp/"):
+    """
+    
+     python karateclub.py run --epoch 1  --debug 0
+    
+
+    """
     g = nx.karate_club_graph()
     nodes = list(g.nodes)
     e1,e2 = zip(*g.edges)
@@ -56,17 +62,42 @@ def run(epoch=2):
     sage.add_edges(e2,e1)
     sage.update_labels(attributes.community)
 
+    ### Debug
     epochs, batch_size = epoch, 15
-    _,_,all_embeddings = sage.train(epochs, batch_size, unsupervised = True, learning_rate=1e-2, 
-                            test_every_n_epochs=10, return_intermediate_embeddings=True)
+    if debug>0:
+        loss_history,perf_history,all_embeddings  = sage.train(epochs, batch_size, unsupervised = True, 
+                                learning_rate=1e-2, 
+                                test_every_n_epochs=10, 
+                                return_intermediate_embeddings=True)
 
-    log(all_embeddings)
+        log(all_embeddings)
+    else: 
+        loss_history,perf_history = sage.train(epochs, batch_size, unsupervised = True, 
+                                learning_rate=1e-2, 
+                                test_every_n_epochs=10, 
+                                return_intermediate_embeddings= False)
+
+        all_embeddings = sage.embeddings
+
+        all_embeddings = np_to_strlist(all_embeddings)
+        dfnode = sage.node_ids
+        dfnode['emb'] = all_embeddings
+        pd_to_file(dfnode, dirout + "/dfemb.parquet", show=1)
+
+
     #animate(sage.labels,all_embeddings,sage.entity_mask)
 
     print(sage.query_neighbors([0,33],k=5))
 
     # sage.start_api()
 
+
+def np_to_strlist(v):
+    v2 = []
+    for vi in v: 
+         si = ",".join([ str(xi) for xi in vi ] )
+         v2.append(si)
+    return v2
 
 
 if __name__=='__main__':
